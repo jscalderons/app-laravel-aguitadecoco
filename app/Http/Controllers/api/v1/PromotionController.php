d6f51e004590b604addca2c5561d3e90fb4ad3f9
Jhonathan Steven Calderon Sanchez <jhonathan19920912@outlook.com>
2019-07-19 10:31:00 -0500

    base

---
+++ app/Http/Controllers/api/v1/PromotionController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use \App\Promotion;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PromotionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response(Promotion::paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $promotion = Promotion::create($request->input());
+
+        return response($promotion);
+    }
+
+    public function attach_product(Request $request)
+    {
+
+    }
+
+    public function detach_product(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::namespace('api\v1')->group(function() {
+    Route::get('/products', 'ProductController@index');
+    Route::post('/product', 'ProductController@store');
+    Route::get('/product/{id}', 'ProductController@show');
+
+    Route::get('/promotions', 'PromotionController@index');
+    Route::post('/promotion', 'PromotionController@store');
+    Route::get('/promotion/{id}', 'PromotionController@show');
 });
